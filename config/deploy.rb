7e7a13bdb0ab5d6dbfef8224b39f8189512b4c30
Julien Minguely <jminguely@gmail.com>
2019-06-18 18:57:34 +0200

    Re-adds opcache clear

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,3 +13,19 @@ set :linked_files, fetch(:linked_files, []).push('.env', 'htdocs/.htaccess')
 set :linked_dirs, fetch(:linked_dirs, []).push('htdocs/content/uploads')
 set :linked_dirs, fetch(:linked_dirs, []).push('storage/framework/sessions')
 
+# In your config/deploy.rb file add the following task
+namespace :deploy do
+	desc 'Create a temporary PHP file to clear the opcache.'
+	task :clear_cache do
+	 	on roles(:app) do
+	 		within fetch(:release_path) do
+				opcache_file = "#{fetch(:release_path)}/htdocs/opcache_clear.php"
+				execute :touch, "#{opcache_file}"
+				execute :chmod, "-R 644 #{opcache_file}"
+				execute :echo, "'<?php opcache_reset(); ?>' > #{opcache_file}"
+				execute :curl, "-s #{fetch(:opcache_file_url)} "
+			end
+		end
+	end
+end
+after 'deploy:finishing', 'deploy:clear_cache'
